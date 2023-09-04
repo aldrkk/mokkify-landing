1ae755363f9a0ff4f2723682c6103bb5f6e6b6cc
Aleksandr Krupoderov <aldr.krup@gmail.com>
2023-09-04 15:33:12 +0600

    change 1 block

--- src/ui/components/Card/index.tsx
+++ src/ui/components/Card/index.tsx
@@ -1,10 +1,9 @@
+import type {FC} from "react"
 import React from "react"
 
-import { Typography } from "@ui/components"
+import {Typography} from "@ui/components"
 
 import style from "./style.module.scss"
-
-import type { FC } from "react"
 
 type Props = {
   imageUrl: string
@@ -12,14 +11,14 @@ type Props = {
   description: string
 }
 
-export const Card: FC<Props> = ({ imageUrl, description, title }) => {
+export const Card: FC<Props> = ({imageUrl, description, title}) => {
   return (
     <div className={style.cardWrapper}>
       <div className={style.cardImage}>
-        <img src={imageUrl} alt="" />
+        <img src={imageUrl} alt=""/>
       </div>
-      <Typography text={title} className={style.cardHeader} type="bold" color="black" />
-      <Typography className={style.cardDescription} text={description} type="light" color="black" />
+      <Typography text={title} className={style.cardHeader} type="bold" color="black"/>
+      <Typography text={description} type="light" color="black"/>
     </div>
   )
 }
